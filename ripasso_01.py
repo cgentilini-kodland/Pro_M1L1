99f66654472fc2d4bb280da2891bb72de133363e
cgentilini-kodland <c.gentilini@kodland.team>
2026-09-11 16:32:05 +0200

    Create ripasso_01.py

---
+++ ripasso_01.py
@@ -0,0 +1,6 @@
+name = input("Come ti chiami?")
+age = int(input("Quanti anni hai?")) # Convertire l'input dell'utente in un numero intero
+
+next_year_age = age + 1
+
+print("Ciao,", name, "avrai ",next_year_age," anni l'anno prossimo!")
